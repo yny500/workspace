0712a0e395acc08649bcbc46eede81d05c75cff1
nayoung <yny500@naver.com>
2024-07-01 17:04:21 +0900

    gulp setting update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,89 +1,84 @@
-import gulp from "gulp"; // plugin 설치 및 가져오기
-import del from "delete";
-import ws from "gulp-webserver";
-import image from "gulp-image";
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
-import fileinclude from "gulp-file-include";
-import miniCSS from "gulp-csso"; // css 압축
-import bro from "gulp-bro"; //  browserify의 신 버전(import, export와 같은 문법은 브라우저가 해석할 수 없기에 해석할 수 있도록 도와줌)
-import babelify from "babelify"; // babel 사용
+import gulp from 'gulp'; // plugin 설치 및 가져오기
+import del from 'delete';
+import ws from 'gulp-webserver';
+import image from 'gulp-image';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import fileinclude from 'gulp-file-include';
+import miniCSS from 'gulp-csso'; // css 압축
+import concat from 'gulp-concat'; // 파일 연결
+import uglify from 'gulp-uglify'; // js 압축
+import babel from 'gulp-babel'; // babel 사용
 
 const routes = {
   html: {
-    watch: "src/**/*.html", // src 폴더 안에 모든 파일을 지켜봄
-    src: "src/*.html", // 해당 경로의 html로 끝나는 파일들(/**/은 폴더 내부까지 포함한단 의미)
-    dest: "dist", // 뿌려줄 경로(폴더, 목적지)
+    watch: 'src/**/*.html', // src 폴더 안에 모든 파일을 지켜봄
+    src: 'src/*.html', // 해당 경로의 html로 끝나는 파일들(/**/은 폴더 내부까지 포함한단 의미)
+    dist: 'dist' // 뿌려줄 경로(폴더, 목적지)
   },
   img: {
-    src: "src/img/*", // src/img 안의 모든 파일(확장자 지정 ex- src/img/*.png)
-    dest: "dist/img",
+    src: 'src/img/*', // src/img 안의 모든 파일(확장자 지정 ex- src/img/*.png)
+    dist: 'dist/img'
   },
   video: {
-    src: "src/video/*",
-    dest: "dist/video",
+    src: 'src/video/*',
+    dist: 'dist/video'
   },
   scss: {
-    watch: "src/scss/**/*.scss",
-    src: "src/scss/style.scss",
-    dest: "dist/css",
+    watch: 'src/scss/**/*.scss',
+    src: 'src/scss/style.scss',
+    dist: 'dist/css'
   },
   js: {
-    watch: "src/js/**/*.js",
-    src: "src/js/main.js",
-    dest: "dist/js",
-  },
+    watch: 'src/js/**/*.js',
+    src: 'src/js/*.js',
+    dist: 'dist/js',
+    minifyFileName: 'main.js'
+  }
 };
 
-const clean = () => del(["dist"]); // 시리즈 추가, 괄호 안에 확장자나 폴더 이름 적어주기
+const clean = () => del(['dist']); // 시리즈 추가, 괄호 안에 확장자나 폴더 이름 적어주기
 
 // 웹 서버 설정
-const webserver = () =>
-  gulp.src("dist").pipe(ws({ livereload: true, open: true })); // 서버에서 보여주고 싶은 폴더 지정
+const webserver = () => gulp.src('dist').pipe(ws({ livereload: true, open: true })); // 서버에서 보여주고 싶은 폴더 지정
 
 const html = () =>
   gulp
     .src(routes.html.src)
     .pipe(
       fileinclude({
-        prefix: "@@",
-        basepath: "@file",
+        prefix: '@@',
+        basepath: '@file'
       })
     )
-    .pipe(gulp.dest(routes.html.dest));
+    .pipe(gulp.dest(routes.html.dist));
 
 const sass = gulpSass(dartSass);
 const styles = () =>
   gulp
     .src(routes.scss.src)
-    .pipe(sass().on("error", sass.logError)) // scss용 에러 설정
+    .pipe(sass().on('error', sass.logError)) // scss용 에러 설정
     .pipe(miniCSS())
-    .pipe(gulp.dest(routes.scss.dest));
+    .pipe(gulp.dest(routes.scss.dist));
 
 const js = () =>
   gulp
     .src(routes.js.src)
-    .pipe(
-      bro({
-        transform: [
-          babelify.configure({ presets: ["@babel/preset-env"] }), // 원하는 프리셋 추가
-          ["uglifyify", { global: true }],
-        ],
-      })
-    )
-    .pipe(gulp.dest(routes.js.dest));
+    .pipe(concat(routes.js.minifyFileName))
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(uglify())
+    .pipe(gulp.dest(routes.js.dist));
 
 const watch = () => {
   gulp.watch(routes.html.watch, html);
+  gulp.watch(routes.img.src, img);
   gulp.watch(routes.scss.watch, styles);
-  gulp.watch(routes.js.src, js);
+  gulp.watch(routes.js.watch, js);
 };
 
-const img = () =>
-  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
+const img = () => gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dist));
 
-const video = () =>
-  gulp.src(routes.video.src).pipe(image()).pipe(gulp.dest(routes.video.dest));
+const video = () => gulp.src(routes.video.src).pipe(image()).pipe(gulp.dest(routes.video.dist));
 
 const prepare = gulp.series([clean, img, video]); // dev 준비 과정에서 발생
 // *ERR_REQUIRE_ESM 오류 발생시
--- src/js/util.js
+++
@@ -1,2 +0,0 @@
-// 최신 자바스크립트 테스트 (일반 브라우저에선 읽을 수 없음)
-export const random = (max) => Math.floor(Math.random() * max);
